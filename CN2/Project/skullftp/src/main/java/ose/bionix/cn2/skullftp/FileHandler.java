1c8d85151226e65dfc69f9bb3153083460f103a6
Bionix Butter <10423123@student.vgu.edu.vn>
2026-05-28 20:26:37 +0700

    CN2/Project/skullftp: v0.1.4 (Fully functional CLI version)

--- CN2/Project/skullftp/src/main/java/ose/bionix/cn2/skullftp/FileHandler.java
+++ CN2/Project/skullftp/src/main/java/ose/bionix/cn2/skullftp/FileHandler.java
@@ -8,11 +8,11 @@ public class FileHandler {
 	private final NetHandler net;
 	public FileHandler(NetHandler net) {this.net = net;}
 
-	// Directory operations
+	// PWD: Print current working directory
 	public String pwd() throws IOException {
 		int respCode = net.sendCmd("PWD");
 		if (respCode != 257) {
-			throw new IOException("Failed to get current working directory");
+			throw new IOException("Failed to get current working directory: " + respCode);
 		}
 		int rsta = net.responseBuffer.indexOf('"');
 		int rend = net.responseBuffer.lastIndexOf('"');
@@ -21,6 +21,8 @@ public class FileHandler {
 		}
 		return null;
 	}
+
+	// LIST is... obviously for listing a directory
 	public List<String> ls() throws IOException {
 		ArrayList<String> files = new ArrayList<String>();
 		String msgFailed = "Failed to list directory, server responded with: ";
@@ -37,83 +39,91 @@ public class FileHandler {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 			String rln; while ((rln = reader.readLine()) != null) {files.add(rln);}
 		} finally {socket.close();}
-
-		// Finally read the completion response (226) from and return the files list
+		// Finally acknowledge the completion response (226) from and return the files list.
 		net.getResponse();
 		return files;
 	}
-	public boolean cd(String dir) throws IOException {
-		int respCode = net.sendCmd("CWD " + dir);
-		return respCode == 250;
-	}
-	public boolean md(String dir) throws IOException { // mkdir
-		int respCode = net.sendCmd("MKD " + dir);
-		return respCode == 257; // 257 "dir" created
-	}
-	public boolean rd(String dir) throws IOException { // rm -r
-		int respCode = net.sendCmd("RMD " + dir);
-		return respCode == 250;
-	}
 
-	// File operations
-	public boolean get(String fsource, String fdest) throws IOException {
+	// RETR and STOR, for downloading and uploading files
+	public void transfer(String mode, String flocal, String fremote) throws IOException {
 		Socket socket = null;
+		String msgFailed = "Failed to download file: ";
+		boolean up = mode.equalsIgnoreCase("put"); // Boolean mode indicator, in favor of upload
+		boolean ok = false;
+		Exception streamException = null;
+
+		// Normally FTP communicates over text-based tunnel mode. If we try to send any raw binary streams
+		// over this our data might get screwed over by text-handling shenanigans (like LE and such).
+		// Thankfully we can tell the server to switch to binary-based tunnel mode instead.
+		int respCode = net.sendCmd("TYPE I");
+		if (respCode != 200) {throw new IOException(msgFailed + respCode);}
 		try {
-			// Normally FTP communicates over text-based tunnel mode. If we try to send any raw binary streams
-			// over this our data might get screwed over by text-handling shenanigans (like LE and such).
-			// Thankfully we can tell the server to switch to binary-based tunnel mode instead.
-			net.sendCmd("TYPE I"); // TODO: See what response code we're gonna get from this mode-switch and handle them accordingly.
 			// We also need a PASV socket to send data over
 			socket = net.requestPasv();
 			// Now we initiate file retrival with RETR
-			int respCode = net.sendCmd("RETR " + fsource);
-			if (respCode != 150 && respCode != 125) {
-				if (socket != null); socket.close();
-				return false;
-			}
+			String cmd = (up ? "STOR " : "RETR ") + fremote;
+			respCode = net.sendCmd(cmd);
+			ok = respCode == 150 || respCode == 125;
+			if (!ok) {throw new IOException(msgFailed + respCode);}
 			// Once server's OK, begin streaming data to fdest
-			try (InputStream strmIn = socket.getInputStream()) {
-				OutputStream strmOut = new FileOutputStream(fdest);
+			// We use the mode indicator to decide which stream to use for both ends
+			try (InputStream strmIn = up ? new FileInputStream(flocal) : socket.getInputStream();
+				OutputStream strmOut = up ? socket.getOutputStream() : new FileOutputStream(flocal)) {
 				byte[] fbuff = new byte[4096];
 				int fbytesRead;
 				while ((fbytesRead = strmIn.read(fbuff)) != -1) {strmOut.write(fbuff, 0, fbytesRead);}
-				strmOut.flush(); strmOut.close();
+				strmOut.flush();
+			} catch (Exception e) {streamException = e;}
+		} finally {
+			if (socket != null && !socket.isClosed()) {try {socket.close();} catch (Exception i) {}}
+			// Finally, like above, acknowledge the completion response (if the transfer did happen, indicated by "ok")
+			// We do this down here to make sure that we don't desync the control channel if "ok" was to be false and an exception is thrown
+			// (Basically leaving the server hanging with a 226/250 and confuse whatever requests that might come next)
+			if (ok) {
+				try { // Handle this too in case if things goes really wrong
+					respCode = net.getResponse();
+					if (streamException == null && respCode != 226 && respCode != 250) {throw new Exception(msgFailed + respCode);}
+				} catch (Exception e) {if (streamException == null) {streamException = e;}}
 			}
-			// Finally, like above, read the completion response
-			net.getResponse();
-			return net.responseBuffer.startsWith("226") || net.responseBuffer.startsWith("250");
-		} catch (IOException e) {return false;}
-		finally {
-			if (socket != null && !socket.isClosed()) {try {socket.close();} catch (Exception e) {}}
 		}
+		if (streamException != null) {throw new IOException(msgFailed + streamException.getMessage());}
 	}
-	public boolean put(String fsource, String fdest) throws IOException {
-		// It's get() but reversed
-		Socket socket = null;
-		try {
-			net.sendCmd("TYPE I"); // TODO: See what response code we're gonna get from this mode-switch and handle them accordingly.
-			socket = net.requestPasv();
-			int respCode = net.sendCmd("STOR " + fdest); // We store with STOR instead of RETR
-			if (respCode != 150 && respCode != 125) {
-				if (socket != null); socket.close();
-				return false;
-			}
-			try (InputStream strmIn = new FileInputStream(fsource)) {
-				OutputStream strmOut =socket.getOutputStream();
-				byte[] fbuff = new byte[4096];
-				int fbytesRead;
-				while ((fbytesRead = strmIn.read(fbuff)) != -1) {strmOut.write(fbuff, 0, fbytesRead);}
-				strmOut.flush(); strmOut.close();
-			}
-			net.getResponse();
-			return net.responseBuffer.startsWith("226") || net.responseBuffer.startsWith("250");
-		} catch (IOException e) {return false;}
-		finally {
-			if (socket != null && !socket.isClosed()) {try {socket.close();} catch (Exception e) {}}
+
+	// CWD, MKD, RMD for manipulating directories, and DELE for deleting files (NOT directories)
+	// These are simpler as all they need to do is send commands with some args.
+	public void sendFileOp(String op, String dir) throws IOException {
+		String cmd;
+		int respCodeExpected;
+		String msgFailed;
+
+		switch (op) {
+			case "cd":
+				cmd = "CWD";
+				respCodeExpected = 250;
+				msgFailed = "Failed to change working directory: ";
+				break;
+			case "mkdir":
+				cmd = "MKD";
+				respCodeExpected = 257;
+				msgFailed = "Failed to create directory: ";
+				break;
+			case "rmdir":
+				cmd = "RMD";
+				respCodeExpected = 250;
+				msgFailed = "Failed to remove directory: ";
+				break;
+			case "del":
+				cmd = "DELE";
+				respCodeExpected = 250;
+				msgFailed = "Failed to delete file: ";
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown file operation specified: " + op);
 		}
-	}
-	public boolean del(String file) throws IOException {
-		int respCode = net.sendCmd("DELE " + file);
-		return respCode == 250;
+
+		int respCode = net.sendCmd(cmd + " " + dir);
+		if (respCode != respCodeExpected) {
+			throw new IOException(msgFailed + respCode);
+		}
 	}
 }
--- CN2/Project/skullftp/src/main/java/ose/bionix/cn2/skullftp/Main.java
+++ CN2/Project/skullftp/src/main/java/ose/bionix/cn2/skullftp/Main.java
@@ -6,12 +6,14 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Console console = System.console();
 		PrintStream output = System.out;
+		PrintStream err = System.err;
 		NetHandler net = new NetHandler();
 		FileHandler file = new FileHandler(net);
 
 		output.println("Project SkullFTP - (c) Bionix Butter");
-		output.println("For testing purposes only. Version 0.1.3.0\n");
+		output.println("For testing purposes only. Version 0.1.4\n");
 
 		// Phase 1: Connect
 		output.print("Enter server IP address: ");
@@ -24,13 +26,13 @@ public class Main {
 			net.connect(ip, port);
 			output.println("Connected! Server Response: " + net.responseBuffer);
 		} catch (IOException e) {
-			System.err.println("Connection failed: " + e.getMessage());
+			err.println("Connection failed: " + e.getMessage());
 			input.close();
 			return;
 		}
 		// Phase 2: Auth
 		try {
-			output.print("Login anonymously? (y/n): ");
+			output.print("Login anonymously? (yN): ");
 			String anon = input.nextLine().trim().toLowerCase();
 			boolean login;
 
@@ -39,20 +41,27 @@ public class Main {
 			} else {
 				output.print("Username: ");
 				String user = input.nextLine().trim();
-				output.print("Password: ");
-				String pass = input.nextLine().trim();
+				String pass = null;
+				if (console != null) {
+					output.print("Password: ");
+					char[] passarr = console.readPassword();
+					pass = String.valueOf(passarr);
+				} else {
+					output.print("Password (WILL BE ECHOED!): ");
+					pass = input.nextLine();
+				}
 				login = net.login(user, pass);
 			}
 
 			if (!login) {
-				System.err.println("Authetication failure.");
+				err.println("Authetication failure.");
 				net.disconnect();
 				input.close();
 				return;
 			}
 			output.println("Ready.");
 		} catch (IOException e) {
-			System.err.println("Authentication failure: " + e.getMessage());
+			err.println("Authentication failure: " + e.getMessage());
 			net.disconnect();
 			input.close();
 			return;
@@ -64,6 +73,7 @@ public class Main {
 			try {
 				cdir = file.pwd();
 			} catch (IOException e) {
+				err.println(e.getMessage());
 				cdir = "?";
 			}
 			// Command parser
@@ -87,68 +97,36 @@ public class Main {
 						break;
 
 					case "cd":
-						if (tokens.length < 2) {
-							output.println("Usage: cd <directory>");
-						} else {
-							if (!file.cd(tokens[1])) {
-								output.println("Failed to change directory.");
-							}
-						}
-						break;
-
 					case "mkdir":
-						if (tokens.length < 2) {
-							output.println("Usage: md <directory_name>");
-						} else {
-							if (!file.md(tokens[1])) {
-								output.println("Failed to create directory.");
-							}
-						}
-						break;
-
 					case "rmdir":
+					case "del":
 						if (tokens.length < 2) {
-							output.println("Usage: rd <directory_name>");
+							String param = "directory";
+							if (command.equals("del")) param = "filename";
+							output.println("Usage: " + command + " <" + param + ">");
 						} else {
-							if (!file.rd(tokens[1])) {
-								output.println("Failed to remove directory.");
-							}
-						}
-						break;
-
-					case "delete":
-						if (tokens.length < 2) {
-							output.println("Usage: del <file_name>");
-						} else {
-							if (!file.del(tokens[1])) {
-								output.println("Failed to delete file.");
-							}
+							file.sendFileOp(command, tokens[1]);
 						}
 						break;
 
 					case "get":
-						if (tokens.length < 3) {
-							output.println("Usage: get <remote_source> <local_destination>");
-						} else {
-							output.println("Downloading...");
-							if (file.get(tokens[1], tokens[2])) {
-								output.println("Download complete.");
-							} else {
-								output.println("Download failed.");
-							}
-						}
-						break;
-
 					case "put":
 						if (tokens.length < 3) {
-							output.println("Usage: put <local_source> <remote_destination>");
+							String usageHint = "Usage: put <local_source> <remote_destination>";
+							if (command.equals("get")) usageHint = "Usage: get <remote_source> <local_destination>";
+							output.println(usageHint);
 						} else {
-							output.println("Uploading...");
-							if (file.put(tokens[1], tokens[2])) {
-								output.println("Upload complete.");
-							} else {
-								output.println("Upload failed.");
+							String actionHint = "Upload";
+							int param1 = 1;
+							int param2 = 2;
+							if (command.equals("get")) {
+								actionHint = "Download";
+								param1 = 2;
+								param2 = 1;
 							}
+							output.println(actionHint + "ing...");
+							file.transfer(command, tokens[param1], tokens[param2]);
+							output.println(actionHint + " complete.");
 						}
 						break;
 
@@ -159,13 +137,10 @@ public class Main {
 						input.close();
 						return;
 					default:
-						output.println("Available commands: pwd, cd <dir>, ls, get <remote> <local>, put <local> <remote>, md <dir>, rmrddir <dir>, del <file>, quit");
+						output.println("Available commands: pwd, cd <dir>, ls, get <remote> <local>, put <local> <remote>, mkdir <dir>, rmdir <dir>, del <file>, quit");
 						break;
 				}
-			} catch (IOException e) {
-				System.err.println("Command execution error: " + e.getMessage());
-				System.err.println("Server connection status might be unstable.");
-			}
+			} catch (IOException e) {err.println(e.getMessage());}
 		}
 	}
 }
